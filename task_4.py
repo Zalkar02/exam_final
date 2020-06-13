d869f47e6dbdec75886a3b57a5ccae7294b91f8b
Zalkar <azisuulu2002@gmail.com>
2020-06-13 19:15:52 +0600

    first commit

--- task_4.py
+++ task_4.py
@@ -1,5 +1,16 @@
 def sum_str(l):
     return sum(map(int, l))
 
+def sum_int(num):
+    output = 0
+    while True:
+        if num < 10:
+            output += num
+            break
+        output += num % 10
+        num //= 10
+    return output
 
-print('Output: ', sum_str(list(input("Input: "))))
+a = input("Input: ")
+print('Output: ', sum_str(list(a)))
+print('Output: ', sum_int(int(a)))
